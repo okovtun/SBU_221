71fa0bdd688bf3bd313857545b0c94818c75a95a
Oleg <31523835+okovtun@users.noreply.github.com>
2022-04-16 11:26:18 +0300

    Arrays DONE

--- Arrays/Arrays/main.cpp
+++ Arrays/Arrays/main.cpp
@@ -6,12 +6,50 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	const int SIZE = 5;
-	int arr[SIZE] = {3, 5, 8};
+	double arr[SIZE] = { 3, 5, 8 };
 	//arr[2] = 3;	//обращаемся ко второму элементу массива, чтобы записать в него значение 3
 	//SIZE - 1 или n - 1
+	/*cout << "Введите элементы массива: ";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cin >> arr[i];
+	}*/
+	int minRand, maxRand;
+	cout << "Введите минимальное случайное число: "; cin >> minRand;
+	cout << "Введите максимальное случайное число: "; cin >> maxRand;
+	minRand *= 10;
+	maxRand *= 10;
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+		arr[i] /= 10;
+		//псевдослучайное число
+		//0 ... 32 767 (RAND_MAX)
+	}
 	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
+	for (int i = SIZE - 1; i >= 0; i--)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	int sum = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Сумма элементов массива: " << sum << endl;
+	cout << "Среднее арифметическое элементов массива: " << (double)sum / SIZE << endl;
+	int min, max;
+	min = max = arr[0];
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)max = arr[i];
+	}
+	cout << "Минимальное значение в массиве: " << min << endl;
+	cout << "Максимальное значение в массиве: " << max << endl;
 }
