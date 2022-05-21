65f58a972b40452329489f186aba2df606229ed8
Oleg <31523835+okovtun@users.noreply.github.com>
2022-05-21 13:37:57 +0300

    Pointers day 1

---
+++ Pointers/DynamicMemory/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер массива: "; cin >> n;
+	int* arr = new int[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
+	
+	delete[] arr;
+}
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//Через арифметику указателей и оператор разыменования:
+		*(arr + i) = rand() % 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//[] - оператор индексирования (subscript operator)
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
