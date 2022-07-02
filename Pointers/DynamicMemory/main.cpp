ab36d3357843de19d3f3a6557f56763177e94a5f
Clayman <Clayman@DESKTOP-DM8E77U>
2022-07-02 12:07:46 +0300

    Extra code renoved

--- Pointers/DynamicMemory/main.cpp
+++ Pointers/DynamicMemory/main.cpp
@@ -250,42 +250,16 @@ template<typename T>T* pop_front(T* arr, int& n)
 template<typename T>T** push_row_down(T** arr, int& rows, const int cols)
 {
 	return push_back(arr, rows, new T[cols]{});
-	/*
-	//1) Создаем буферный массив указателей:
-	T** buffer = new T*[rows + 1]{};
-	//2) Копируем адреса строк из исходного массива указателей в буферный:
-	for (int i = 0; i < rows; i++)
-	{
-		buffer[i] = arr[i];
-	}
-	//3) Удавляем исходный массив:
-	delete[] arr;
-	//4) Подменяем адрес старого массива адресом нового массива:
-	arr = buffer;
-	//5) Создаем добавляемую строку:
-	arr[rows] = new T[cols] {};
-	//6) После добавления строки, количество строк массива увеличивается на одну:
-	rows++;
-	return arr;
-	*/
 }
 template<typename T>T** pop_row_down(T** arr, int& rows, const int cols)
 {
 	delete[] arr[rows - 1];
 	return pop_back(arr, rows);
-	/*T** buffer = new T*[--rows]{};
-	for (int i = 0; i < rows; i++)buffer[i] = arr[i];
-	delete[] arr;
-	return buffer;*/
 }
 template<typename T>void push_col_right(T** arr, const int rows, int& cols)
 {
 	for (int i = 0; i < rows; i++)
 	{
-		/*T* buffer = new T[cols + 1]{};
-		for (int j = 0; j < cols; j++)buffer[j] = arr[i][j];
-		delete[] arr[i];
-		arr[i] = buffer;*/
 		arr[i] = push_back(arr[i], cols, T());//T() - значение по умолчанию для шаблонного типа
 		cols--;	//Компенсируем увеличение cols функцией push_back
 	}
